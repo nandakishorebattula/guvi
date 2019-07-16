610bb5ea1575814eff91488fd2fb7e1a5fc297a2
Mohana Nanda Kishore Battula <51699652+nandakishorebattula@users.noreply.github.com>
2019-07-16 09:21:20 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
+
 int main()
 {
     char c;
-    int isLowercaseVowel, isUppercaseVowel;
     scanf("%c",&c);
-    isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-    isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
-    if (isLowercaseVowel || isUppercaseVowel)
-        printf("Vowel");
-    else if((c >= 65 && c<= 92) || (c>=97 && c<=123))
-        printf("Consonant");
-    else 
-        printf("invalid");
+    if((c=='a')||(c=='e')||(c=='i')||(c=='o')||(c=='u'))
+    printf("Vowel");
+    else if(c>='a' && c<='z')
+    printf("Consonant");
+    else
+    printf("Invalid");
     return 0;
 }
