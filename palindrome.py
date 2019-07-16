38c98f67bddd822e66b97f97a971f61d081280a3
Mohana Nanda Kishore Battula <51699652+nandakishorebattula@users.noreply.github.com>
2019-07-16 11:14:23 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -2,10 +2,10 @@ a=int(input())
 temp=a
 rev=0
 while(a>0):
-    dig=a%10
-    rev=rev*10+dig
+    digit=a%10
+    reverse=reverse*10+digit
     a=a//10
-if(temp==rev):
+if(temp==reverse):
     print("yes")
 else:
     print("no")
