3609836ec66ee8cb4b0b030cf88e93ccac057b5b
Mohana Nanda Kishore Battula <51699652+nandakishorebattula@users.noreply.github.com>
2019-07-16 13:17:23 +0530

    Update pro2.py

--- pro2.py
+++ pro2.py
@@ -1,13 +1,14 @@
-p,q=input().strip().split(" ")
-q=int(b)
-sum=0
-while(sum<len(p)-1 and q):
-  if(p[sum]>p[sum+1]):
-    q-=1
-    p=p[:sum]+p[sum+1:]
-    if(sum!=0):
-      sum-=1
-  else:
-    sum+=1
-d=p[:len(p)-q]
-print(d)
+    
+a,b=input().strip().split(" ")
+b=int(b)
+i=0
+while i<len(a)-1 and b:
+	if(a[i]>a[i+1]):
+		b-=1
+		a=a[:i]+a[i+1:]
+		if(i!=0):
+			i-=1
+	else:
+		i+=1
+v=n[:len(a)-b]
+print(v)
