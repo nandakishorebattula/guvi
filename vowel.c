9ff8d4140928608a35e3ccbb8574396474ea483c
Mohana Nanda Kishore Battula <51699652+nandakishorebattula@users.noreply.github.com>
2019-07-16 09:14:23 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -5,7 +5,7 @@ int main()
     int isLowercaseVowel, isUppercaseVowel;
     scanf("%c",&c);
     isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-    isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+    isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
     if (isLowercaseVowel || isUppercaseVowel)
         printf("Vowel");
     else
