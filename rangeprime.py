b994f0208f9843d2bfeb00b5bea4d5f075a62044
Mohana Nanda Kishore Battula <51699652+nandakishorebattula@users.noreply.github.com>
2019-07-16 13:08:28 +0530

    Create rangeprime.py

---
+++ rangeprime.py
@@ -0,0 +1,8 @@
+a,b = map(int,input().split())
+for c in range(a,b + 1):  
+   if c > 1:  
+       for i in range(2,c):  
+           if (c % i) == 0:  
+               break  
+       else:  
+           print(c, end=" ") 
